7ba13e63b229821e4144bae35c98b40ebaf9d9f7
Matheus Bodra <matheusbodra@gmail.com>
2022-06-24 13:39:22 -0300

    mudando css

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -9,7 +9,6 @@ import '../styles/pages/leaderboard.css';
 const Leaderboard = () => {
   const [logged, setLogin] = useState(false);
   const [currentFilter, setCurrentFilter] = useState('Classificação Geral');
-  // const [alert, setAlert] = useState(true)
 
   useEffect(() => {
     const user = localStorage.getItem('user');
@@ -23,7 +22,7 @@ const Leaderboard = () => {
     <>
       <div className="alert">
         <span className="closebtn" onClick={ ({ target }) => target.parentElement.style.display = 'none' }>&times;</span>
-          Frontend desenvolvido por <a href="https://www.betrybe.com/" className='alert-link'>Trybe</a>. Backend desenvolvido por <a href="https://www.linkedin.com/in/matheus-bodra/" className='alert-link'>Matheus Bodra</a>. Para mais informações <a href="https://github.com/MBodra/mbodra-trybe-futebol-clube/tree/master#projeto-trybe-futebol-clube-%EF%B8%8F" className='alert-link'>leia o README do projeto</a>.
+          Frontend desenvolvido por <a href="https://www.betrybe.com/" className='alert-link'>Trybe</a>. Backend desenvolvido por <a href="https://www.linkedin.com/in/matheus-bodra/" className='alert-link'>Matheus Bodra</a>. Para mais informações leia o <a href="https://github.com/MBodra/mbodra-trybe-futebol-clube/tree/master#projeto-trybe-futebol-clube-%EF%B8%8F" className='alert-link'>README</a> do projeto.
       </div>
       <Header
         page="CLASSIFICAÇÃO"
